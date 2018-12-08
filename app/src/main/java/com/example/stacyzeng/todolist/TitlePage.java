bdba8ea97c09f0cd414feed67a7386cec9223a5d
smzeng <stacymz2@illinois.edu>
2018-12-08 14:53:53 -0600

    UI complete

---
+++ app/src/main/java/com/example/stacyzeng/todolist/TitlePage.java
@@ -0,0 +1,26 @@
+package com.example.stacyzeng.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class TitlePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_page);
+
+        final Button button = findViewById(R.id.startButton);
+        button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(final View view) {
+            Intent i = new Intent(TitlePage.this, MainActivity.class);
+            startActivity(i);
+        }
+    });
+}
+
+}
---
+++ app/src/main/java/com/example/stacyzeng/todolist/addList.java
@@ -0,0 +1,14 @@
+package com.example.stacyzeng.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addList extends AppCompatActivity {
+    String listName = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_list);
+    }
+
+}
